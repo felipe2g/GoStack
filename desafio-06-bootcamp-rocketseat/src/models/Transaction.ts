4775d063586239d04ca1cec45d2222195029affa
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-08-02 13:54:03 -0300

    added Category and Transaction Entity models

--- desafio-06-bootcamp-rocketseat/src/models/Transaction.ts
+++ desafio-06-bootcamp-rocketseat/src/models/Transaction.ts
@@ -1,16 +1,32 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('varchar')
   title: string;
 
+  @Column('varchar')
   type: 'income' | 'outcome';
 
+  @Column('money')
   value: number;
 
+  @Column('varchar')
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 

ef581c54ee51d939b3f621ba531b7869d73f2021
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-08-04 23:01:40 -0300

    initial development of csv reading

--- desafio-06-bootcamp-rocketseat/src/routes/transactions.routes.ts
+++ desafio-06-bootcamp-rocketseat/src/routes/transactions.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
-
-import { getCustomRepository, getRepository } from 'typeorm';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
-import Transaction from '../models/Transaction';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+import uploadConfig from '../config/upload';
+
+import multer = require('multer');
+
+const upload = multer(uploadConfig);
 
 const transactionsRouter = Router();
 
@@ -43,8 +46,18 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.json(deletedTransaction);
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransaction = new ImportTransactionsService();
+
+    const importedTransactions = importTransaction.execute(
+      request.file.filename,
+    );
+
+    return response.json(importedTransactions);
+  },
+);
 
 export default transactionsRouter;
--- desafio-06-bootcamp-rocketseat/src/services/ImportTransactionsService.ts
+++ desafio-06-bootcamp-rocketseat/src/services/ImportTransactionsService.ts
@@ -1,8 +1,31 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import path from 'path';
+
 import Transaction from '../models/Transaction';
+import uploadConfig from '../config/upload';
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filename: string): Promise<Transaction[]> {
+    const csvFilePath = path.resolve(
+      __dirname,
+      uploadConfig.directory,
+      filename,
+    );
+
+    const readCSVStream = fs.createReadStream(csvFilePath);
+
+    const parseStream = csvParse({
+      from_line: 2,
+      ltrim: true,
+      rtrim: true,
+    });
+
+    const parseCSV = readCSVStream.pipe(parseStream);
+
+    parseCSV.on('data', line => {
+      console.log(line);
+    });
   }
 }
 

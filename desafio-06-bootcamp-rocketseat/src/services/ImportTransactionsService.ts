a5f792823405185d4c0e05665b2cd4d8008a3467
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-08-06 04:09:44 -0300

    finishing the csv import not done

--- desafio-06-bootcamp-rocketseat/src/services/ImportTransactionsService.ts
+++ desafio-06-bootcamp-rocketseat/src/services/ImportTransactionsService.ts
@@ -1,9 +1,14 @@
+import { getRepository } from 'typeorm';
+
 import csvParse from 'csv-parse';
 import fs from 'fs';
 import path from 'path';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 import uploadConfig from '../config/upload';
+
+import CreateTransactionService from './CreateTransactionService';
 
 class ImportTransactionsService {
   async execute(filename: string): Promise<Transaction[]> {
@@ -23,8 +28,51 @@ class ImportTransactionsService {
 
     const parseCSV = readCSVStream.pipe(parseStream);
 
-    parseCSV.on('data', line => {
-      console.log(line);
+    const transactions = [];
+    const categories: string[] = [];
+
+    parseCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+
+      if (!title || !type || !value) return;
+
+      categories.push(category);
+      transactions.push({ title, type, value, category });
+    });
+
+    await new Promise(resolve => parseCSV.on('end', resolve));
+
+    const categoryRepository = await getRepository(Category);
+
+    const uniqueCategory = Array.from(new Set(categories));
+
+    await uniqueCategory.map(async field => {
+      try {
+        const categoryExist = await categoryRepository.find({
+          where: {
+            title: field,
+          },
+        });
+
+        console.log(categoryExist.length);
+
+        if (categoryExist.length === 0) {
+          const newCategory = await categoryRepository.create({ title: field });
+          await categoryRepository.save(newCategory);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    transactions.forEach(async operation => {
+      const createTransaction = new CreateTransactionService();
+
+      const transaction = await createTransaction.execute(operation);
+
+      console.log(transaction);
     });
   }
 }

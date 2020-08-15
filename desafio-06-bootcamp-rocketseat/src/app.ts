5b1fb8850385378720209d46ba5e9e07f1ddec1d
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-08-15 02:12:17 -0300

    some changes to work in challenge 07

--- desafio-06-bootcamp-rocketseat/src/app.ts
+++ desafio-06-bootcamp-rocketseat/src/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import 'dotenv/config';
+import cors from 'cors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
@@ -14,6 +15,7 @@ import createConnection from './database';
 createConnection();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
